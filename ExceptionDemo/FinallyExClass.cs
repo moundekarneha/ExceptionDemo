323b357261b9ae9737ef2cca77daab3ccc569db8
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 17:43:14 +0530

    Add project files.

---
+++ ExceptionDemo/FinallyExClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    internal class FinallyExClass
+    {
+        //public static void Main()
+        //{
+        //    try
+        //    {
+
+        //    }
+        //    catch (Exception e)
+        //    {
+
+                
+        //    }
+        //}
+    }
+}
---
+++ ExceptionDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter first num: ");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter first num: ");
+                int y = int.Parse(Console.ReadLine());
+
+                //if y =1 then it will jump to end of this static main() method but in same case also it will execute finally block
+                //- this is a speciality of finally block
+                if(y==1)
+                {
+                    return;
+                }
+
+                int z = x / y;
+                Console.WriteLine("\nResult = " + z);
+            }
+            catch (DivideByZeroException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            catch (FormatException e)
+            {
+
+                Console.WriteLine("Input must be numeric = " + e.Message);
+            }
+
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally block will execute in both cases whether the exception is occur or not");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
